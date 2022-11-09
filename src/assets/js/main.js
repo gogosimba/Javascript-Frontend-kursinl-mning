77f27831111dc028f46c7b3dff6e1b1dceb4b3df
gogosimba <111742024+gogosimba@users.noreply.github.com>
2022-11-09 14:23:28 +0100

    Formatted the readme, Added function to find product name and price and add it to the cart. Now I just have to add the information to a localstorage and display it at pageload.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,24 +1,25 @@
 const api_url = "https://api.punkapi.com/v2/beers?page=2&per_page=80";
 const newBeers = document.getElementById("newbeers");
+const emptycart = document.getElementById("emptycart");
 const gallery = document.querySelector('.productcont');
 const addtocartBtns = document.getElementsByClassName('addtocart');
+const items = { ...localStorage };
 let apiData = [];
 let lastBeer = 0;
+let cartOutput = document.getElementById('carttable');
 let output = "";
 
 window.onload = () => {
     loadBeers();
     addListeners();
+    updateCart();
 }
 
 
 addListeners = function () {
     for (var i = 0; i < addtocartBtns.length; i++) {
-        addtocartBtns[i].addEventListener('click', function() {addToCart(this);});
+        addtocartBtns[i].addEventListener('click', function () { addToCart(this); });
     }
-}
-addToCart = function(elem){
-    
 }
 
 //Laddar in API till apiData så att fetch inte körs mer än en gång och uppdaterar sidan
@@ -33,23 +34,23 @@ loadBeers = function () {
 
 //Hämtar data från apiData och visar på skärmen. Använder finalBeer för att minnas var loopen slutade och fortsätter därifrån för att få nya öl
 getBeers = function () {
-    if (lastBeer === apiData.length -1) {
+    if (lastBeer === apiData.length - 1) {
         lastBeer = 0;
     }
     let beerName, beerImg, beerDescription, beerPrice, beerVolume, finalBeer;
-    finalBeer = lastBeer + 12;
+    finalBeer = lastBeer + 6;
     for (i = lastBeer; i < finalBeer; i++) {
-        try{
-        beerName = apiData[i].name;
-        beerImg = apiData[i].image_url;
-        beerDescription = apiData[i].description;
-        beerPrice = apiData[i].ebc;
-        beerVolume = apiData[i].volume;
-        lastBeer = i;
+        //Försöker fånga ett error om API'et tar slut under loopen och sätter då lastBeer till 0 och börjar om från början.
+        try {
+            beerName = apiData[i].name;
+            beerImg = apiData[i].image_url;
+            beerDescription = apiData[i].description;
+            beerPrice = apiData[i].ebc;
+            beerVolume = apiData[i].volume;
+            lastBeer = i;
         }
-        catch(TypeError)
-        {
-            lastBeer=0;
+        catch (TypeError) {
+            lastBeer = 0;
         }
         output += `<div class="product">
                 <h3 class="productname">${beerName}</h3>
@@ -73,4 +74,28 @@ newBeers.addEventListener("click", function () {
     getBeers();
 })
 
+addToCart = function(elem){
+    let getprice;
+    let getproductName;
+    while(elem = elem.previousSibling) {
+        if (elem.nodeType === 3) continue; // Skippar textnoder
+        if(elem.className == "price"){
+            getprice = elem.innerText;
+        }
+        if (elem.className == "productname") {
+            getproductName = elem.innerText;
+        }
+        sibs.push(elem);
+    }
+    cartOutput.innerHTML += "<tr><td>" + getproductName + "<td>" +getprice;
+    console.log(sibs);
+    console.log(getprice);
+    console.log(getproductName);
+}
 
+updateCart = function(){
+
+        itemString = JSON.stringify(items);
+        itemString = itemString.replace(/[|&;$%@"<>()+,]/g,"");
+        console.log(itemString);
+}
